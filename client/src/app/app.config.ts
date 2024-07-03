03374d353b277c26d4b890e50b9c2ba4ed89638b
rev7 <20608265+rev-07@users.noreply.github.com>
2024-07-02 23:52:07 -0400

    Remove unused provider

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -4,14 +4,12 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
 import { provideHttpClient } from '@angular/common/http';
-import { provideOAuthClient } from 'angular-oauth2-oidc';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(),
-    provideOAuthClient(),
     provideAnimationsAsync(),
   ],
 };
