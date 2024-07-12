cce3bcd2f8172d7afd97be272b04f448a501321b
Mario Liao <mario@Marios-MacBook-Pro.local>
2024-07-12 12:36:19 -0400

    added scoring system

--- client/src/app/game/game.component.ts
+++ client/src/app/game/game.component.ts
@@ -16,15 +16,29 @@ export class GameComponent {
     player1Writer!: HanziWriter;
     player2Writer!: HanziWriter;
     character: string = '我';
+    characterStrokes: number = 7;
 
     // Default values for game page
     isGameStarted: boolean = false;
+    gameResultsHidden: boolean = true;
 
     // Values for the game
     timer: number = 30;
     difficulty: number = 1;
     showOutline: boolean = false;
     showHintAfterMisses: number = 0;
+
+    // Scoring for the game
+    player1Result: string = '';
+    player1Score: number = 0;
+    player1StrokesFinished: number = 0;
+    player1MistakesMade: number = 0;
+    player1TimeFinished: number = 0;
+    player2Result: string = '';
+    player2Score: number = 0;
+    player2StrokesFinished: number = 0;
+    player2MistakesMade: number = 0;
+    player2TimeFinished: number = 0;
 
     constructor(private router: Router) { }
 
@@ -92,15 +106,36 @@ export class GameComponent {
     startTimer() {
       var timerInterval = setInterval( () => {
         this.timer--;
-        if (this.timer <= 0) {
+        if (this.timer <= 0 || (this.player1TimeFinished > 0 && this.player2TimeFinished > 0)) {
           clearInterval(timerInterval);
+          this.declareWinner();
         }
       }, 1000);
     }
 
     startQuiz() {
-      this.player1Writer.quiz({});
-      this.player2Writer.quiz({});
+      this.player1Writer.quiz({
+        onMistake: () => {
+          this.player1MistakesMade++;
+        },
+        onCorrectStroke: () => {
+          this.player1StrokesFinished++;
+        },
+        onComplete: () => {
+          this.player1TimeFinished = this.timer;
+        }
+      });
+      this.player2Writer.quiz({
+        onMistake: () => {
+          this.player2MistakesMade++;
+        },
+        onCorrectStroke: () => {
+          this.player2StrokesFinished++;
+        },
+        onComplete: () => {
+          this.player2TimeFinished = this.timer;
+        }
+      });
     }
 
     setProperGameValues() {
@@ -111,6 +146,31 @@ export class GameComponent {
         this.showOutline = true;
       } else {
         this.showOutline = false;
+      }
+    }
+
+    declareWinner() {
+      this.gameResultsHidden = false;
+      this.player1Score = (this.player1StrokesFinished * 50) + (this.player1TimeFinished * 10) - (this.player1MistakesMade * 20);
+      this.player2Score = (this.player2StrokesFinished * 50) + (this.player2TimeFinished * 10) - (this.player2MistakesMade * 20);
+      if (this.player1StrokesFinished === this.characterStrokes && this.player2StrokesFinished < this.characterStrokes) {
+        this.player1Result = 'Victory'
+        this.player2Result = 'Defeat'
+      } else if (this.player2StrokesFinished === this.characterStrokes && this.player1StrokesFinished < this.characterStrokes) {
+        this.player1Result = 'Defeat'
+        this.player2Result = 'Victory'
+      }
+      else {
+        if (this.player1Score  === this.player2Score) {
+          this.player1Result = 'Tie'
+          this.player2Result = 'Tie'
+        } else if (this.player1Score  > this.player2Score) {
+          this.player1Result = 'Victory'
+          this.player2Result = 'Defeat'
+        } else {
+          this.player1Result = 'Defeat'
+          this.player2Result = 'Victory'
+        }
       }
     }
 }
