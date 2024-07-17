07952499f6d7c7d3f33f8925b1a6dd19b94831dc
Frans Budiman <105959450+fransbudiman@users.noreply.github.com>
2024-07-17 16:00:00 -0400

    add character model (#13)
    * add character model
    
    * add character selection screen

---
+++ server/db/models/character.js
@@ -0,0 +1,38 @@
+import { Model, DataTypes } from "sequelize";
+
+export default (sequelize) => {
+    class Character extends Model{
+        static associate(models){
+            //Character.belongsToMany(models.User, { foreignKey: "userId", as: "user" });
+            //implement for the characters that are already learned feature or something else
+        }
+    }
+
+    Character.init({
+        id:{
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        character:{
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        pinyin:{
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        english:{
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+
+    },
+    {
+        sequelize,
+        modelName: "Character",
+    }
+    );
+    return Character;
+}
+
